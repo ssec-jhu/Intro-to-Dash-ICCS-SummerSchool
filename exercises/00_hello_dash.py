0ed0b2117dff416ab6af9ab7b8276d1127c28d47
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2026-05-18 10:50:26 -0400

    initial add of the files (#1)

---
+++ exercises/00_hello_dash.py
@@ -0,0 +1,26 @@
+# This is a simple Dash application that displays 
+# "Hello Dash" as a heading. To run this code, 
+# make sure you have the Dash library installed in 
+# your Python environment. 
+# You can install required dependencies using the requirements.txt 
+# file provided in the repository: 
+# run `pip install -r requirements.txt` in your terminal.
+from dash import Dash, html
+
+# Create a Dash application instance - consider this as the main 
+# entry point of your Dash app.
+app = Dash(__name__)
+
+# Create the layout of the app using HTML components provided by Dash.
+app.layout = html.Div(
+    [
+        # Define what coponents will be displayed on the webpage. 
+        # In this case, we are displaying a simple heading.
+        html.H1("Hello Dash")
+    ])
+
+if __name__ == "__main__":
+    # setting debug=True will allow the app to automatically 
+    # reload when you make changes to the code. 
+    # This is useful during development.
+    app.run(debug=True)
